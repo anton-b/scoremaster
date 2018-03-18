41120fe93974f718ca1ac56452bcb7f870cfe52d
Anton Benkevich <abenkevich@alertlogic.com>
2018-03-18 15:17:22 +0000

    Storage index

--- src/storage.cpp
+++ src/storage.cpp
@@ -1,23 +1,15 @@
 #include "storage.h"
 #include <Arduino.h>
+#include <string.h>
 
-Storage::Storage()
+
+
+
+Storage::Storage(const char *nspace)
 {
-    if (nvs_flash_init() == ESP_OK)
-    {
-        Serial.println("NVS init ok");
-        if (nvs_open("matches", NVS_READWRITE, &__st_handle) == ESP_OK)
-        {
-            Serial.println("NVS open ok");
-        } else
-        {
-            Serial.println("NVS open failed");
-        }
-    } else
-    {
-      Serial.println("NVS init failed");
-    }
-
+    handle_nvs_error(nvs_flash_init());
+    handle_nvs_error(nvs_open(nspace, NVS_READWRITE, &__st_handle));
+    ensure_index();
 };
 
 Storage::~Storage()
@@ -25,41 +17,171 @@ Storage::~Storage()
     nvs_close(__st_handle);
 };
 
-void Storage::save(const char * key, matchrecord * mr) 
+void Storage::add_element(int id, void *data, size_t size)
 {
-    size_t size = sizeof(matchrecord);
-    esp_err_t t = nvs_set_blob(__st_handle, key, mr, size);
-    t = nvs_commit(__st_handle);
-    Serial.print(t);
+    if (element_exists(id) == false)
+    {
+        write(make_key(id), data, size);
+        add_to_index(id);
+        commit_index();
+    } else 
+    {
+        throw "element_exists";
+    };
 };
 
-matchrecord * Storage::read(const char * key)
+void Storage::get_element(int id, void * result)
+{
+    if (element_exists(id))
+    {
+        read(make_key(id), result);
+    };
+}
+
+std::vector<int> Storage::list_elements()
+{
+    return __index;
+};
+
+void Storage::remove_element(int id)
+{
+    clear(make_key(id));
+    remove_from_index(id);
+    commit_index();
+};
+
+void Storage::remove_all()
+{
+    // Just clear all namespace keys including index
+    clear_all();
+};
+
+void Storage::write(const char *key, void *data, size_t size)
+{
+    handle_nvs_error(nvs_set_blob(__st_handle, key, data, size));
+    handle_nvs_error(nvs_commit(__st_handle));
+};
+
+void Storage::read(const char *key, void *result)
+{
+    size_t size = key_size(key);
+    handle_nvs_error(nvs_get_blob(__st_handle, key, result, &size));
+};
+
+size_t Storage::key_size(const char *key)
 {
     size_t size = 0;
-    matchrecord * mr = new matchrecord;
-    esp_err_t err;
-    err = nvs_get_blob(__st_handle, key, NULL, &size);
-    if (err != ESP_OK) Serial.println("Error");
-    Serial.println(size);
-    err = nvs_get_blob(__st_handle, key, mr, &size);
-    if (err != ESP_OK) Serial.println("Error");
-    return mr;
-};
-
-int Storage::size()
-{
-    return 0;
+    handle_nvs_error(nvs_get_blob(__st_handle, key, NULL, &size));
+    return size;
 };
 
 void Storage::clear_all()
 {
-    nvs_erase_all(__st_handle);
-    nvs_commit(__st_handle);
+    handle_nvs_error(nvs_erase_all(__st_handle));
+    handle_nvs_error(nvs_commit(__st_handle));
 };
 
-void Storage::clear(const char * key)
+void Storage::clear(const char *key)
 {
-    nvs_erase_key(__st_handle, key);
-    nvs_commit(__st_handle);
-}
+    handle_nvs_error(nvs_erase_key(__st_handle, key));
+    handle_nvs_error(nvs_commit(__st_handle));
+};
 
+size_t Storage::length()
+{
+    return __index.size();
+};
+
+void Storage::add_to_index(int id)
+{
+    __index.push_back(id);
+};
+
+void Storage::remove_from_index(int id)
+{
+    for (std::vector<int>::iterator i = __index.begin(); i != __index.end(); ++i)
+    {
+        if (*i = id)
+        {
+            __index.erase(i);
+        };
+    };
+    commit_index();
+};
+
+bool Storage::element_exists(int id)
+{
+    return (find(__index.begin(), __index.end(), id) != __index.end());
+};
+ 
+void Storage::commit_index()
+{
+    if (__index.size() > 0)
+    {
+        write("__index__", &__index[0], sizeof(int) * __index.size());
+    } else
+    {
+        clear("__index__");
+    };
+};
+
+void Storage::ensure_index()
+{
+    try
+    {
+        int index_size = key_size("__index__") / sizeof(int);
+        int *index = new int[index_size];
+        read("__index__", index);
+        __index = std::vector<int>(index, index + index_size);
+        delete[] index;
+    }
+    catch (const char *e)
+    {
+        return;
+    }
+};
+
+const char *Storage::make_key(int id)
+{
+    String key = String(id, 10);
+    return key.c_str();
+};
+
+void Storage::handle_nvs_error(esp_err_t error)
+{
+    switch (error)
+    {
+    case ESP_OK:
+        break;
+    //init errors
+    case ESP_ERR_NVS_NO_FREE_PAGES:
+        throw "no_free_pages";
+    //open errors
+    case ESP_ERR_NVS_NOT_INITIALIZED:
+        throw "not_initialized";
+    case ESP_ERR_NVS_PART_NOT_FOUND:
+        throw "part_not_found";
+    // read and common errors
+    case ESP_ERR_NVS_INVALID_NAME:
+        throw "invalid_name";
+    case ESP_ERR_NVS_INVALID_LENGTH:
+        throw "invalid_length";
+    case ESP_ERR_NVS_NOT_FOUND:
+        throw "not_found";
+    case ESP_ERR_NVS_INVALID_HANDLE:
+        throw "invalid_handle";
+    // write errors
+    case ESP_ERR_NVS_READ_ONLY:
+        throw "nvs_readonly";
+    case ESP_ERR_NVS_NOT_ENOUGH_SPACE:
+        throw "not_enough_space";
+    case ESP_ERR_NVS_REMOVE_FAILED:
+        throw "nvs_remove_failed";
+    case ESP_ERR_NVS_KEY_TOO_LONG:
+        throw "key_too_long";
+    case ESP_ERR_NVS_VALUE_TOO_LONG:
+        throw "value_too_long";
+    default:
+        throw "unknown_error";
+    };
+};
--- src/storage.h
+++ src/storage.h
@@ -3,18 +3,37 @@
 #include "types.h"
 #include "nvs.h"
 #include "nvs_flash.h"
+#include <vector>
+#include <algorithm>
 
-class Storage 
+class Storage
 {
-    public:
-        Storage();
-        ~Storage();
-        void save(const char * key, matchrecord * mr);
-        matchrecord * read(const char * key);
-        void clear(const char * key);
-        void clear_all();
-        int size();
-    private:
-        nvs_handle __st_handle;
+  public:
+    Storage(const char *nspace);
+    ~Storage();
+    void add_element(int id, void *data, size_t size);
+    void get_element(int id, void * result);
+    std::vector<int> list_elements();
+    void remove_element(int id);
+    void remove_all();
+    bool element_exists(int id);
+  
+    size_t length();
+  protected:
+    /*Underlyng memory manipulation*/
+    void handle_nvs_error(esp_err_t error);
+    void write(const char *key, void *data, size_t size);
+    void read(const char *key, void *result);
+    void clear(const char *key);
+    void clear_all();
+    /* Index manipulation */
+    void ensure_index();
+    void add_to_index(int id);
+    void remove_from_index(int id);
+    void commit_index();
+    size_t key_size(const char *key);
+    const char *make_key(int id);
+    nvs_handle __st_handle;
+    std::vector<int> __index;
 };
 #endif
